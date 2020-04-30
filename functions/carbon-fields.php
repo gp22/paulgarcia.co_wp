6a5462627cd2327972823f93c32e565dc98ac544
gp22 <gp22@tutanota.com>
2020-04-30 14:47:17 -0600

    initial commit

---
+++ functions/carbon-fields.php
@@ -0,0 +1,65 @@
+<?php
+/**
+ * Setup Carbon Fields
+ *
+ * @package      ClientName
+ * @author       Paul Garcia
+ * @since        1.0.0
+ * @license      GPL-2.0+
+**/
+
+
+use Carbon_Fields\Container;
+use Carbon_Fields\Field;
+
+function crb_attach_post_options() {
+    Container::make( 'post_meta', 'Landing Page Sections' )
+        ->where( 'post_template', '=', 'template-landing-page.php' )
+        ->add_fields( array(
+            Field::make( 'complex', 'crb_sections' )
+                // Hero
+                ->add_fields( 'hero', 'Hero', array(
+										Field::make( 'text', 'hero_heading', 'Hero Heading' ),
+										Field::make( 'text', 'hero_subhead', 'Hero Subhead' ),
+										Field::make( 'image', 'hero_image', 'Hero Image' ),
+								) )
+
+                // Feature list ( "what you'll get" ) section
+                ->add_fields( 'feature_list', 'Feature List', array(
+										Field::make( 'complex', 'features', 'Features' )
+											->add_fields( array(
+												Field::make( 'text', 'feature_title', 'Feature Title' ),
+												Field::make( 'text', 'feature_body', 'Feature Body' ),
+											) ),
+                ) )
+
+                // Second group will be a list of files for users to download
+                // ->add_fields( 'file_list', 'File List', array(
+                //     Field::make( 'complex', 'files', 'Files' )
+                //         ->add_fields( array(
+								// 						Field::make( 'text', 'title', 'Title' ),
+                //             Field::make( 'file', 'file', 'File' ),
+                //         ) ),
+                // ) )
+
+                // Third group will be a list of manually selected posts
+                // used as a simple curated "Related posts" listing
+                // ->add_fields( 'related_posts', 'Related Posts', array(
+                //     Field::make( 'association', 'posts', 'Posts' )
+                //         ->set_types( array(
+                //             array(
+                //                 'type' => 'post',
+                //                 'post_type' => 'post',
+                //             ),
+                //         ) ),
+                // ) ),
+        ) );
+}
+add_action( 'carbon_fields_register_fields', 'crb_attach_post_options' );
+
+
+function crb_load() {
+	require_once( WP_PLUGIN_DIR . '/vendor/autoload.php' );
+	\Carbon_Fields\Carbon_Fields::boot();
+}
+add_action( 'after_setup_theme', 'crb_load' );
---
+++ webpack.config.js
@@ -0,0 +1,93 @@
+const path = require('path');
+const glob = require('glob');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
+const StyleLintPlugin = require('stylelint-webpack-plugin');
+const PurgecssPlugin = require('purgecss-webpack-plugin');
+
+module.exports = (env, argv) => ({
+  context: __dirname,
+  entry: {
+    frontend: ['./src/index.js', './src/css/style.css', './src/sass/style.scss'],
+    customizer: './src/customizer.js'
+  },
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: '[name]-bundle.js'
+  },
+  mode: 'development',
+  devtool: 'cheap-eval-source-map',
+  module: {
+    rules: [
+      {
+        enforce: 'pre',
+        exclude: /node_modules/,
+        test: /\.jsx$/,
+        loader: 'eslint-loader'
+      },
+      {
+        test: /\.jsx?$/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.s?css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          {
+            loader: 'postcss-loader',
+            options: {
+              ident: 'postcss',
+              plugins: [require('tailwindcss'), require('autoprefixer')]
+            }
+          },
+          'sass-loader'
+        ]
+      },
+      {
+        test: /\.svg$/,
+        loader: 'svg-sprite-loader',
+        options: {
+          extract: true,
+          spriteFilename: 'svg-defs.svg'
+        }
+      },
+      {
+        test: /\.(jpe?g|png|gif)\$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              outputPath: 'images/',
+              name: '[name].[ext]'
+            }
+          },
+          'img-loader'
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new StyleLintPlugin(),
+    new MiniCssExtractPlugin({
+      filename: '[name].css'
+    }),
+    new BrowserSyncPlugin({
+      files: '**/*.php',
+      proxy: 'http://paulgarciaco.local/'
+    }),
+    ...(argv.mode === 'production'
+      ? [
+          new PurgecssPlugin({
+            paths: glob.sync(`${path.resolve(__dirname)}/**/*.php`, { nodir: true }),
+            defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+          })
+        ]
+      : [])
+  ],
+  optimization: {
+    minimizer: [new UglifyJsPlugin(), new OptimizeCssAssetsPlugin()]
+  }
+});
