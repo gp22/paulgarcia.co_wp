02cb4b5640b4b0551eb93e2f268344a57eac08a7
gp22 <gp22@tutanota.com>
2020-05-25 16:32:53 -0600

    add edit and save functions

--- inc/gutenberg.php
+++ inc/gutenberg.php
@@ -5,7 +5,7 @@ function pgarcia_gutenberg_blocks() {
 	wp_register_script(
 		'custom-cta-js',
 		get_template_directory_uri() . '/public/gutenberg-cta-block-bundle.js',
-		array( 'wp-blocks' )
+		array( 'wp-blocks', 'wp-block-editor', 'wp-components' )
 	);
 
 	register_block_type(
--- src/js/gutenberg/gutenberg-cta-block.js
+++ src/js/gutenberg/gutenberg-cta-block.js
@@ -1,13 +1,111 @@
-wp.blocks.registerBlockType('pgarcia/custom-cta', {
+/**
+ * Custom Gutenberg CTA Block
+ * JavaScript dependencies defined in /inc/gutenberg.php
+ *
+ * @package      ClientName
+ * @author       Paul Garcia
+ * @since        1.0.0
+ * @license      GPL-2.0+
+ **/
+
+const { registerBlockType } = wp.blocks;
+const { RichText, InspectorControls, ColorPalette, MediaUpload } = wp.blockEditor;
+const { PanelBody, IconButton } = wp.components;
+
+registerBlockType('pgarcia/custom-cta', {
   title: 'Call to Action',
   description: 'Generate CTA',
   icon: 'format-image',
   category: 'layout',
-  attributes: {},
-  edit: props => {
-    return <div></div>;
+  attributes: {
+    title: {
+      type: 'string',
+      source: 'html',
+      selector: 'h2'
+    },
+    titleColor: {
+      type: 'string',
+      default: 'black'
+    },
+    body: {
+      type: 'string',
+      source: 'html',
+      selector: 'p'
+    },
+    backgroundImage: {
+      type: 'string',
+      default: null
+    }
   },
-  save: props => {
-    return <div></div>;
+  edit: ({ attributes, setAttributes }) => {
+    const { title, body, titleColor, backgroundImage } = attributes;
+
+    function onChange(type, value) {
+      setAttributes({ [type]: value });
+    }
+
+    function onTitleColorChange(newColor) {
+      setAttributes({ titleColor: newColor });
+    }
+
+    function onSelectImage(newImage) {
+      setAttributes({ backgroundImage: newImage.sizes.full.url });
+    }
+
+    return [
+      <InspectorControls>
+        <PanelBody title="Font Color Settings">
+          <p>
+            <strong>Select a Title color:</strong>
+          </p>
+          <ColorPalette value={titleColor} onChange={onTitleColorChange} />
+        </PanelBody>
+        <PanelBody title="Background Image Settings">
+          <MediaUpload
+            onSelect={onSelectImage}
+            type="image"
+            value={backgroundImage}
+            render={({ open }) => (
+              <IconButton onClick={open} icon="upload">
+                Background Image
+              </IconButton>
+            )}
+          />
+        </PanelBody>
+      </InspectorControls>,
+      <div className="cta-container">
+        <RichText
+          key="editable"
+          tagName="h2"
+          placeholder="Your CTA Title"
+          value={title}
+          onChange={e => onChange('title', e)}
+          style={{ color: titleColor }}
+        />
+        <RichText
+          key="editable"
+          tagName="p"
+          placeholder="Your CTA Description"
+          value={body}
+          onChange={e => onChange('body', e)}
+        />
+      </div>
+    ];
+  },
+  save: ({ attributes }) => {
+    const { title, body, titleColor } = attributes;
+
+    return (
+      <div className="cta-container">
+        <p></p>
+        <ul>
+          <li>
+            <p>lajskldfjlasfdlkas</p>
+          </li>
+        </ul>
+        <RichText.Content tagName="h2" value={title} style={{ color: titleColor }} />
+        <RichText.Content tagName="p" value={body} />
+      </div>
+    );
   }
 });
